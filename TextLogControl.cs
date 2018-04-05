3517b46ff36a09b626c64e3b6541a0ace91bb3f2
danielchen09 <danielchen@pacificamerican.org>
2018-04-05 16:32:41 +0800

    Create TextLogControl.cs

---
+++ TextLogControl.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextLogControl : MonoBehaviour {
+	public GameObject textTemplate;
+	private List<GameObject> textItems = new List<GameObject> ();
+
+	void Start (){
+		Debug.Log ("ASDGFADSVGVSFAVSAD");
+	}
+
+	public void logText(string text, Color color){
+
+		if (textItems.Count == 30) {
+			GameObject tmp = textItems [0];
+			Destroy (tmp.gameObject);
+			textItems.Remove (tmp);
+		}
+
+		GameObject newText = Instantiate (textTemplate) as GameObject;
+		newText.SetActive (true);
+
+		newText.GetComponent<TextLogItem> ().setText (text, color);
+		newText.transform.SetParent (textTemplate.transform.parent, false);
+
+		textItems.Add (newText.gameObject);
+	}
+}
